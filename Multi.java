a5fa13282e0629798198b3fa3807c5d26a0daf77
Bhagyarathi123 <116142525+Bhagyarathi123@users.noreply.github.com>
2022-11-20 14:06:30 +0530

    Add files via upload

---
+++ Multi.java
@@ -0,0 +1,15 @@
+public class Multi {
+	 int x,y,z;
+	public void multi() {
+		x=5;y=10;
+		z=x*y;
+		System.out.println("Multi of x and y is:"+z);
+	}
+
+	public static void main(String[] args) {
+	Multi z=new Multi();
+	z.multi();
+
+	}
+
+}
